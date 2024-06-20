cff91c6e00145d9f865b5d8904247fef506ff9b4
Francesco Puppo <francescopuppo@me.com>
2024-06-20 19:20:32 +0200

    Some personalisation

--- src/consts.ts
+++ src/consts.ts
@@ -1,8 +1,8 @@
 import type { Site, Metadata, Socials } from "@types";
 
 export const SITE: Site = {
-  NAME: "Astro Nano",
-  EMAIL: "markhorn.dev@gmail.com",
+  NAME: "Francesco Puppo",
+  EMAIL: "francescopuppo@me.com",
   NUM_POSTS_ON_HOMEPAGE: 3,
   NUM_WORKS_ON_HOMEPAGE: 2,
   NUM_PROJECTS_ON_HOMEPAGE: 3,
@@ -30,8 +30,8 @@ export const PROJECTS: Metadata = {
 
 export const SOCIALS: Socials = [
   { 
-    NAME: "twitter-x",
-    HREF: "https://twitter.com/markhorn_dev",
+    NAME: "mastodon",
+    HREF: "https://mastodon.com",
   },
   { 
     NAME: "github",
